327173afb1a578c2d8fba2ee50d92c84a00f5a68
giamann1 <145729311+giamann1@users.noreply.github.com>
2024-01-29 16:10:54 +0000

    completed

---
+++ M2T1_Manning.cpp
@@ -0,0 +1,32 @@
+/*
+CSC 134
+M2T1 - Receipt
+Name
+1/29/24
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout << "M2T1" << end1;
+    // set up variables
+    string meal = "Value Meal";
+    // all in $, except the percent
+    double meal price = 5.99;
+    double tax_percent = 0.08;
+    double tax_amount;
+    double total;
+
+    // do calculations
+
+    // print the receipt
+    // TODO: make it line up, and fix decimals
+    cout << meal << meal_price;
+    cout << "tax" << tax_amount;
+    cout << "total" << total;
+    
+    return 0;
+}
