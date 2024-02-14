96440def129da262d932aa378c32480ccdab6ee8
giamann1 <145729311+giamann1@users.noreply.github.com>
2024-02-14 16:12:50 +0000

    done

---
+++ m3/M3T1_Manning.cpp
@@ -0,0 +1,32 @@
+
+// the header
+// M3T1
+// Gia Manning
+// 2-14-2024
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    // Declare variables
+    double length1, width1;
+    double length2, width2;
+    double area1, area2;
+
+    // Ask for length and width of first rectangele
+    cout << "Length of the first rectangele: ";
+    cin >> length1;
+    cout << "Width of first rectangele: ";
+    cin >> width1;
+    cout << "Length of second rectangele: ";
+    cin >> length2;
+    cout << "Width of second rectangele: ";
+    cin >> width2;
+
+    // Calculate the areas
+    area1 = length1 * width1;
+    area2 = length2 * width2;
+    // Print the areas
+    
+    return 0;
+}
